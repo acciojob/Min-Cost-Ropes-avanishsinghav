7979864744956ee18825d01bbee88017f4a8ca9f
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-05 19:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if(arr.length <= 1){
+		return 0;
+	}
+
+	const temp = [...arr].sort((a,b)=> a-b);
+	let totalcost = 0
+
+	while(temp.length > 1){
+		const first = temp.shift();
+		const second = temp.shift();
+		const cost = first+second;
+		totalcost += cost;
+
+		let num = false;
+		for(let i = 0; i<temp.length; i++){
+			if(cost < temp[i]){
+				temp.splice(i,0,cost);
+				num = true;
+				break;
+			}
+		}
+		if(!num)temp.push(cost);
+	}
+	return totalcost;
 }
 
 module.exports=mincost;
